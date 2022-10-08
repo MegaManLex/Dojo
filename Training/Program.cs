081389fee366808fc548b788488b6d787f39d7d3
MegaManLex <pabril93@outlook.com>
2022-10-08 10:47:48 -0400

    Add project files.

---
+++ Training/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Training
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            
+            string name, color, pluralNoun, celebrity;
+           
+
+            Console.WriteLine("Hey! What's your name?...");
+            name = Console.ReadLine();
+
+            Console.ForegroundColor= ConsoleColor.Blue;
+            Console.WriteLine("favorite color? :");
+            color = Console.ReadLine();
+
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("What are you? :");
+            pluralNoun = Console.ReadLine();
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("who's you're favorite celebrity? : ");
+            celebrity = Console.ReadLine();
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Well it's nice to meet you " + name + ". My favorite color is actually red, not " + color + " I'm also an octopus not a " + pluralNoun + ". Why is " + celebrity + " your favorite famouse person?");
+        }
+    }
+}
