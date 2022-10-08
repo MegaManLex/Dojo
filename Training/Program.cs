801ce4cf50fedb985dfa908e988dfb6edec27911
MegaManLex <pabril93@outlook.com>
2022-10-08 11:44:23 -0400

    Update Program.cs

--- Training/Program.cs
+++ Training/Program.cs
@@ -6,19 +6,26 @@ using System.Threading.Tasks;
 
 namespace Training
 {
-    class Program
+    class Dojo
     {
         static void Main(string[] args)
         {
+            MadLib();
+
+            Console.ReadLine();
+        }
+
+        static void MadLib()
+        {
             Console.ForegroundColor = ConsoleColor.Green;
-            
+
             string name, color, pluralNoun, celebrity;
-           
+
 
             Console.WriteLine("Hey! What's your name?...");
             name = Console.ReadLine();
 
-            Console.ForegroundColor= ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.Blue;
             Console.WriteLine("favorite color? :");
             color = Console.ReadLine();
 
@@ -31,7 +38,9 @@ namespace Training
             celebrity = Console.ReadLine();
 
             Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.WriteLine("Well it's nice to meet you " + name + ". My favorite color is actually red, not " + color + " I'm also an octopus not a " + pluralNoun + ". Why is " + celebrity + " your favorite famouse person?");
+            Console.WriteLine(" Well it's nice to meet you " + name + ". \n My favorite color is actually red, not " + color + " \n I'm also an octopus not a " + pluralNoun + ". \n Why is " + celebrity + " your favorite famouse person?");
         }
     }
+
 }
+
